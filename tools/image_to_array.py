afdacef6a47d483e60d6baa27f0b0dea987a052d
KirillPogranichny <kirillpogranichny@gmail.com>
2023-05-05 17:22:35 +1000

    add resize images function

--- tools/image_to_array.py
+++ tools/image_to_array.py
@@ -1,26 +1,30 @@
-from skimage import io
-import cv2
-import numpy as np
+from matplotlib import pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
 import os
 
-filelist = "../RAP_dataset"
+
+def plotImage(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
 
 
-def image2array():
-    image_array = []
-    for image in os.listdir(filelist):
-        img = io.imread(image)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (224, 224))
-        image_array.append(img)
-    image_array = np.array(image_array)
-    image_array = image_array.reshape(image_array.shape[0], 224, 224, 3)
-    image_array = image_array.astype('float32')
-    image_array /= 255
-    return np.array(image_array)
+if __name__ == "__main__":
+    train_list = os.path.join('C:/Study/RAP-course-work/RAP_dataset/')
 
+    BATCH_SIZE = 10
+    IMG_SHAPE = 100
 
-train_data = image2array()
-print("Length of training dataset:", train_data.shape)
+    train_image_generator = ImageDataGenerator(rescale=1. / 255)
 
-# print(os.listdir(filelist))
+    train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                               directory=train_list,
+                                                               shuffle=True,
+                                                               target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                               class_mode='binary')
+
+    sample_training_images, _ = next(train_data_gen)
+    plotImage(sample_training_images)
