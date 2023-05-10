15655741e4d71066ee3803e63e8fd4b9269d88d6
KirillPogranichny <kirillpogranichny@gmail.com>
2023-05-10 12:29:03 +1000

    update .gitignore

--- tools/image_to_array.py
+++ tools/image_to_array.py
@@ -1,16 +1,18 @@
 from PIL import Image
 import numpy as np
 import os
+import sys
 
+
+np.set_printoptions(threshold=sys.maxsize)
 path = os.listdir("../dataset")
 img_list = np.zeros((len(path)), dtype=object)
 print(img_list.size)
 
 img_iter = 0
 
-for element in path:
+for element in path[:1]:
     img_list[img_iter] = np.pad(np.asarray(Image.open('../dataset/' + element).resize((155, 155)).convert('RGB')), pad_width=1)
     img_iter += 1
 
 print(img_list)
-
