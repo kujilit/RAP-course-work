96992c87274679f030a341efff2be5aaa64a2f6b
Mike Litvin <mrlitvinm@gmail.com>
2023-06-23 17:25:49 +1000

    some changes

--- tools/image_to_array.py
+++ tools/image_to_array.py
@@ -6,35 +6,47 @@ from PIL import Image
 import numpy as np
 import os
 
-path = os.listdir("../dataset")
-img_list = np.empty((len(path)), dtype=object)
-transform = transforms.ToTensor()
+class CNN:
+    def __init__(self, path):
 
-padding = torch.nn.ConstantPad2d(1, 0)
+        global image
+        self.path = os.listdir("../dataset")
+        self.img_list = np.empty((len(self.path)), dtype=object)
+        transform = transforms.ToTensor()
 
-img_iter = 0
+        padding = torch.nn.ConstantPad2d(1, 0)
 
-kernel = torch.tensor([[
-    [0, -1, 0],
-    [-1, 1, -1],
-    [0, -1, 0]]], dtype=torch.float32)
-conv = torch.nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3)
-with torch.no_grad():
-    conv.weight.data.copy_(kernel)
+        self.img_iter = 0
 
-for element in path:
-    image = Image.open('../dataset/' + element).resize((255, 255))
-    img_list[img_iter] = image
-    img_list[img_iter] = padding(transform(img_list[img_iter]))
+        kernel = torch.tensor([[
+            [0, -1, 0],
+            [-1, 1, -1],
+            [0, -1, 0]]], dtype=torch.float32)
+        conv = torch.nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3)
 
-    try:
-        img_list[img_iter] = conv(img_list[img_iter].unsqueeze(0))
-    except:
-        print("Ошибка применения свертки:", traceback.format_exc())
+        with torch.no_grad():
+            conv.weight.data.copy_(kernel)
 
-    img_iter += 1
+        for element in self.path:
+            if element[0] == '.':
+                continue
+            try:
+                image = Image.open('../dataset/' + element).resize((255, 255))
+            except:
+                print("Не удалось привести изображение к необходимому формату:", traceback.format_exc())
 
-print(img_list.shape)
-img_list[0].unsqueeze(0)
-output_img = transforms.ToPILImage()(img_list[0][0])
-output_img.save("test.jpg")
+            self.img_list[self.img_iter] = image
+            self.img_list[self.img_iter] = padding(transform(self.img_list[self.img_iter]))
+
+            try:
+                self.img_list[self.img_iter] = conv(self.img_list[self.img_iter].unsqueeze(0))
+            except:
+                print("Ошибка применения свертки:", traceback.format_exc())
+            self.img_iter += 1
+
+    def get_conv_images(self):
+        return self.img_list
+
+    def __len__(self):
+        return self.img_iter
+
