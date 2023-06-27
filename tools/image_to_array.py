c939659be0eb08af33c968db158e6ce740d872ec
KirillPogranichny <kirillpogranichny@gmail.com>
2023-06-27 16:43:34 +1000

    fix CNN

--- tools/image_to_array.py
+++ tools/image_to_array.py
@@ -7,6 +7,10 @@ from PIL import Image
 import numpy as np
 import os
 import sys
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 
 
 # torch.set_printoptions(threshold=sys.maxsize)
@@ -34,10 +38,18 @@ class CNN:
             [0, -1, 0],
             [-1, 3, -1],
             [0, -1, 0]]], dtype=torch.float32)
+        # kernel_2 = torch.tensor([[
+        #     [1, 2, 1],
+        #     [2, 4, 2],
+        #     [1, 2, 1]]], dtype=torch.float32)
         conv = torch.nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3)
+        # conv_2 = torch.nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3)
+        relu = torch.nn.LeakyReLU(0.2, inplace=True)
+        max_pool = torch.nn.MaxPool2d(2, 2)
 
         with torch.no_grad():
             conv.weight.data.copy_(kernel)
+            # conv_2.weight.data.copy_(kernel_2)
 
         for element in self.path:
             if element[0] == '.':
@@ -52,6 +64,9 @@ class CNN:
 
             try:
                 self.img_list[self.img_iter] = conv(self.img_list[self.img_iter].unsqueeze(0))
+                # self.img_list[self.img_iter] = (1 / 16) * conv_2(self.img_list[self.img_iter].unsqueeze(0))
+                self.img_list[self.img_iter] = relu(self.img_list[self.img_iter])
+                self.img_list[self.img_iter] = max_pool(self.img_list[self.img_iter])
                 self.data_table[self.img_iter][0] = element
                 self.data_table[self.img_iter][1] = self.img_list[self.img_iter]
                 signs = []
@@ -72,13 +87,21 @@ class CNN:
                 print("Ошибка применения свертки:", traceback.format_exc())
             self.img_iter += 1
 
-        for i in range(5):
+        column_names = ['Index', 'Name', 'Backpack', 'ShoulderBag', 'HandBag',
+                        'WaistBag', 'Box', 'PlasticBag', 'PaperBag']
+        df.to_csv('dataframe.csv', index=False, header=column_names, na_rep='Unknown', )
+
+        for i in range(50, 60):
             self.img_list[i].unsqueeze(0)
             output_img = transforms.ToPILImage()(self.img_list[i][0])
-            output_img.save(f"test{i}.jpg")
+            output_img.save(f"test{60-i}.jpg")
 
     def get_conv_images(self):
         return self.data_table
 
     def __len__(self):
         return self.img_iter
+
+
+path = os.listdir("../dataset")
+dataset = CNN(path)
--- tools/split_dataset.py
+++ tools/split_dataset.py
@@ -4,10 +4,12 @@ import numpy as np
 import torch
 import sys
 from sklearn.model_selection import train_test_split
+from datetime import datetime
 
 
 # torch.set_printoptions(threshold=sys.maxsize)
 # np.set_printoptions(threshold=sys.maxsize)
+start_time = datetime.now()
 
 
 torch.set_printoptions(threshold=sys.maxsize)
@@ -18,14 +20,18 @@ dataset = CNN(path)
 
 data_len = dataset.__len__()
 dataset = dataset.get_conv_images()
+#
+# x_dataset = []
+# y_dataset = []
 
-x_dataset = []
-y_dataset = []
-
-for i in range(data_len):
-    x_dataset.append(dataset[i][1])
-    y_dataset.append(dataset[i][2])
-
-x_train, x_test, y_train, y_test = train_test_split(x_dataset, y_dataset)
-
-print(len(x_train), len(y_train), len(x_test), len(y_test))
+# for i in range(data_len):
+#     x_dataset.append(dataset[i][1])
+#     y_dataset.append(dataset[i][2])
+#
+# x_dataset = np.asarray(x_dataset)
+# y_dataset = np.asarray(y_dataset)
+#
+# x_train, x_test, y_train, y_test = train_test_split(x_dataset, y_dataset)
+#
+# print(len(x_train), len(y_train), len(x_test), len(y_test))
+print(f"Время работы алгоритма: {datetime.now() - start_time}")
